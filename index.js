593017271741c1589548a86daa52d230a879a5f9
Rob Sindorf <robert.sindorf@gmail.com>
2021-05-16 23:31:33 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+    return string.toUpperCase("hello");
+}
+function whisper(string) {
+    return string.toLowerCase("hello");
+}
+function logShout(string) {
+    console.log(string.toUpperCase("hello"));
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase("hello"));
+}
+function sayHiToGrandma(string) {
+    if (string == string.toLowerCase()) {
+        return 'I can\'t hear you!';
+    }   else if (string == string.toUpperCase()) {
+        return 'YES INDEED!';
+    }   else if (string == "I love you, Grandma.") {
+        return 'I love you, too.';
+    }}
